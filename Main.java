51c8e350e5d9604ec45d867ee93623b4c218e2aa
Mateus Nascimento <124949062+MateusJn@users.noreply.github.com>
2023-09-26 23:38:41 -0300

    Envio dos arquivos.

---
+++ Main.java
@@ -0,0 +1,13 @@
+package PraticaHeranca;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Secretario s1 = new Secretario();
+		s1.setNome("Mateus Nascimento");
+		System.out.println(s1.getNome());
+
+	}
+
+}
---
+++ Secretario.java
@@ -0,0 +1,15 @@
+package PraticaHeranca;
+
+public class Secretario extends Funcionario {
+	
+	int numeroDeRamal;
+
+	public int getNumeroDeRamal() {
+		return numeroDeRamal;
+	}
+
+	public void setNumeroDeRamal(int numeroDeRamal) {
+		this.numeroDeRamal = numeroDeRamal;
+	}
+	
+}
